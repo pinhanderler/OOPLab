0c464fd634bbed3bf1e52fb5a3422cad2248d81e
Pinhan <pinhanderler@gmail.com>
2021-05-11 17:25:59 +0300

    Update TheMain.java

--- src/lab6/TheMain.java
+++ src/lab6/TheMain.java
@@ -1,4 +1,4 @@
-package lab6;
+package Lab6V2;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -14,38 +14,38 @@ public class TheMain {
         //увімкнення приладів в розетку
 
         int answer;
-        try (Scanner in = new Scanner(System.in)) {
-			System.out.println("Які електроприлади ви хочете увімкнути у розетку?\nВведіть 1, якщо хочете увімкнути " +
-			        "холодильник; 2 - якщо хочете увімкнути праску; 3 - якщо хочете увімкнути сірий ноутбук; 4 - " +
-			        "якщо хочете увімкнути чорний ноутбук.");
-			while (in.hasNextInt()){
-			    answer = in.nextInt();
-			    if (answer < listOfElApp.size() && answer > 0)
-			        listOfElApp.get(answer - 1).setpluggedIn();
-			    else
-			        System.out.println("Значення некоректне.");
-			    System.out.println("Введіть 1, якщо хочете увімкнути" +
-			            "холодильник; 2 - якщо хочете увімкнути праску; 3 - якщо хочете увімкнути ноутбук.");
-			}
-			System.out.println("Включення в розетку завершене!");
-			//підрахування споживчої потужності
-			in.next();
-			int minFrequency = 0, maxFrequency = 100;
-			System.out.println("Задайте діапазон електромагнітного випромінення.\nМінімальна частота ν = ");
-			if (in.hasNextInt())
-			    minFrequency = in.nextInt();
-			else in.next();
-			System.out.println("Максимальна частота ν = ");
-			if (in.hasNextInt())
-			    maxFrequency = in.nextInt();
-			else in.next();
-			//Провести сортування приладів у квартирі на основі потужності.
-			listOfElApp.sort(Comparator.comparing(ElectricAppliances::getPower));
-			for (int i = 0; i < listOfElApp.size(); i++) {
-			    System.out.println(listOfElApp.get(i).outputAppByFrequency(maxFrequency, minFrequency));
-			    System.out.println(listOfElApp.get(i).toString());
-			}
-		}
+        Scanner in = new Scanner(System.in);
+        System.out.println("Які електроприлади ви хочете увімкнути у розетку?\nВведіть 1, якщо хочете увімкнути " +
+                "холодильник; 2 - якщо хочете увімкнути праску; 3 - якщо хочете увімкнути сірий ноутбук; 4 - " +
+                "якщо хочете увімкнути чорний ноутбук.");
+        while (in.hasNextInt()){
+            answer = in.nextInt();
+            if (answer < listOfElApp.size() && answer > 0)
+                listOfElApp.get(answer - 1).setpluggedIn();
+            else
+                System.out.println("Значення некоректне.");
+            System.out.println("Введіть 1, якщо хочете увімкнути" +
+                    "холодильник; 2 - якщо хочете увімкнути праску; 3 - якщо хочете увімкнути ноутбук.");
+        }
+        System.out.println("Включення в розетку завершене!");
+        //підрахування споживчої потужності
+        in.next();
+        int minFrequency = 0, maxFrequency = 100;
+        System.out.println("Задайте діапазон електромагнітного випромінення.\nМінімальна частота ν = ");
+        if (in.hasNextInt())
+            minFrequency = in.nextInt();
+        else in.next();
+        System.out.println("Максимальна частота ν = ");
+        if (in.hasNextInt())
+            maxFrequency = in.nextInt();
+        else in.next();
+        //Провести сортування приладів у квартирі на основі потужності.
+        listOfElApp.sort(Comparator.comparing(ElectricAppliances::getPower));
+        for (int i = 0; i < listOfElApp.size(); i++) {
+            System.out.println(listOfElApp.get(i).outputAppByFrequency(maxFrequency, minFrequency));
+            System.out.println(listOfElApp.get(i).toString());
+        }
+
         System.out.println("Done!");
     }
 }
