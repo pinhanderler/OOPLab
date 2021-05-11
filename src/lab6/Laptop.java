19a597a6fac95c814eb1e51d288b2105a249bf6a
Pinhan <pinhanderler@gmail.com>
2021-05-11 17:24:37 +0300

    Update Laptop.java

--- src/lab6/Laptop.java
+++ src/lab6/Laptop.java
@@ -1,7 +1,9 @@
-package lab6;
+package Lab6V2;
 
 public class Laptop extends ElectricAppliances {
+    private float screenSize;
     public Laptop(String n, int p, float w, String c, String m, int f, float s){
         super(n, p, w, c, m, f);
+        screenSize = s;
     }
 }
